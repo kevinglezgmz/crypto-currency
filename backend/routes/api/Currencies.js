b15771e3186336e44a5588fb39cf511b8c06e481
kevinglezgmz <44679178+kevinglezgmz@users.noreply.github.com>
2020-06-10 10:52:14 -0500

    feat: added design and functionality to the /prices page (#7)
    * change: made frontend work with delay
    
    * feat: added functionality to the /prices page

--- backend/routes/api/Currencies.js
+++ backend/routes/api/Currencies.js
@@ -5,8 +5,12 @@ const axios = require('axios')
 
 // Creating route to get all the coins in the nomics database
 router.get('/', async (req, res) => {
-  const response = await axios.get(`https://api.nomics.com/v1/currencies?key=${key}&attributes=id`)
-  res.status(200).json(response.data)
+  const response = await axios.get(`https://api.nomics.com/v1/currencies?key=${key}&attributes=id,cryptocontrol_coin_id`)
+  res.status(200).json(
+    response.data.filter((coinData) => {
+      return coinData.cryptocontrol_coin_id !== null
+    }),
+  )
 })
 
 module.exports = router
--- frontend/components/CurrencySelector.tsx
+++ frontend/components/CurrencySelector.tsx
@@ -1,80 +1,61 @@
 import React from 'react'
 import styles from './CurrencySelector.module.scss'
-import axios from 'axios'
 import Link from 'next/link'
+import CoinHolder from './CoinHolder'
+import SearchBar from './SearchBarSuggestions'
 
-type ResponseData = {
-  id: string
+type PropsType = {
+  props: {
+    coinsData: { coin: string; checked: boolean }[]
+    setCoinsData: React.Dispatch<
+      React.SetStateAction<
+        {
+          coin: string
+          checked: boolean
+        }[]
+      >
+    >
+  }
 }
 
-export const CurrencySelector: React.FC = () => {
+export default function CurrencySelector({ props }: PropsType) {
   const [state, setState] = React.useState(false)
-  const [data, setData] = React.useState([])
-  const [clickedCoins, setCoins] = React.useState(new Map<string, string>())
-  const [query, setQuery] = React.useState('')
+  const [query, setQuery] = React.useState({ query: '' })
+  const { coinsData, setCoinsData } = props
+  const [coinSuggestions, setSuggestions] = React.useState<{ coin: string; checked: boolean }[]>([])
 
   const showBox = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
     e.preventDefault()
     setState(!state)
   }
 
-  const checkBoxClick = (e: React.MouseEvent<HTMLInputElement, MouseEvent> | any) => {
-    if (!clickedCoins.get(e.target.id)) clickedCoins.set(e.target.id, e.target.id)
-    else clickedCoins.delete(e.target.id)
-    let s: string = ''
-    clickedCoins.forEach((v, k) => {
-      s = s + v + ','
-    })
-    setQuery(s)
-  }
-
-  const renderCoins = () => {
-    return data.length > 0 ? (
-      <>
-        {data.map((element: string) => (
-          <label key={element} htmlFor={element}>
-            <input onClick={checkBoxClick} type="checkbox" id={element} />
-            {element}
-          </label>
-        ))}
-      </>
-    ) : (
-      <div>Loading...</div>
-    )
-  }
-
-  React.useEffect(() => {
-    async function fetchData() {
-      const res = await axios.get('http://localhost:5001/api/currencies')
-      setData(res.data.map((val: ResponseData) => val.id).filter((val: string) => val.length === 3))
-    }
-    if (data.length === 0) {
-      fetchData()
-    }
-  })
-
   return (
-    <>
-      <form>
-        <div className={styles.multiselect}>
-          <div className={styles.selectBox}>
-            <div className={styles.buttonWrapper}>
-              <button type="button" className="btn btn-primary btn-lg btn-block" onClick={showBox}>
-                Show cryptos
-              </button>
-            </div>
-            <div className={styles.buttonWrapper}>
-              <Link href={`/prices?coins=${query}`}>
-                <a className="btn btn-primary btn-lg btn-block multiselect">Check prices</a>
-              </Link>
-            </div>
+    <form>
+      <div className={styles.multiselect}>
+        <div className={styles.selectBox}>
+          <div className={styles.buttonWrapper}>
+            <button type="button" className="btn btn-primary btn-lg btn-block" onClick={showBox}>
+              {!state ? 'Show all coins' : 'Hide coins'}
+            </button>
           </div>
-
-          <div className={styles.checkboxes} style={{ display: state ? 'block' : 'none' }}>
-            {renderCoins()}
+          <div className={styles.buttonWrapper}>
+            <Link href={`/prices?coins=${query.query}`}>
+              <a className="btn btn-primary btn-lg btn-block multiselect">Check prices</a>
+            </Link>
           </div>
         </div>
-      </form>
-    </>
+        <div className={styles.coinsAndFilter}>
+          <CoinHolder
+            query={query}
+            setQuery={setQuery}
+            state={state}
+            coinSuggestions={coinSuggestions}
+            coinsData={coinsData}
+            setCoinsData={setCoinsData}
+          />
+          <SearchBar props={{ coinsData, state, coinSuggestions, setSuggestions }} />
+        </div>
+      </div>
+    </form>
   )
 }
